acb9c2f43b568e2e58e2bdd4d4d9f670086e5000
max-b10 <bungaymax@gmail.com>
2021-03-17 20:32:13 +0000

    Update functions.js

--- scripts/functions.js
+++ scripts/functions.js
@@ -1,7 +1,14 @@
 // Player Score Display:
 const p1DTC = () => {
-  playerOneDisplay.textContent = `${playerOneScore}`;
+  playerOneDisplay.textContent = `Player 1: ${playerOneScore}`;
 };
 const p2DTC = () => {
-  playerTwoDisplay.textContent = `${playerTwoScore}`;
+  playerTwoDisplay.textContent = `Player 2: ${playerTwoScore}`;
 };
+function reset() {
+  playerOneScore = 0;
+  playerTwoScore = 0;
+  isSeriesFinish = false;
+  p1DTC();
+  p2DTC();
+}
